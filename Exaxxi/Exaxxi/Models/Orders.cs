3d827c04924cf2873fcc2e876a92657d05bfcdab
khoa <dodangkhoagtvt@gmail.com>
2019-07-08 11:42:10 +0700

    add table Orders.cs in db

---
+++ Exaxxi/Exaxxi/Models/Orders.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exaxxi.Models
+{
+    public class Orders
+    {
+        [Key]
+        public int id { get; set; }
+        [Display(Name = "Start Time")]
+        public DateTime time { get; set; }
+        [Required]
+        [Display(Name = "Delivery Address")]
+        public string address { get; set; }
+        [Required]
+        [Phone]
+        [Display(Name = "Phone")]
+        public string phone { get; set; }
+        [Display(Name = "Status")]
+        public int status { get; set; }
+
+        public int id_user { get; set; }
+        [ForeignKey("id_user")]
+        public Users petitioner { get; set; }
+        public int id_post { get; set; }
+        [ForeignKey("id_post")]
+        public Posts post { get; set; }
+    }
+}
