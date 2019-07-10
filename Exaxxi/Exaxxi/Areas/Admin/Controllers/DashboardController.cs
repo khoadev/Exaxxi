8874bfaae77e0ed560bffaeba42aeaa386d01bb9
kietnguyen161 <kietnguyen161@gmail.com>
2019-07-10 09:27:19 +0700

    add dashboard and login admin

---
+++ Exaxxi/Exaxxi/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exaxxi.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            if (String.IsNullOrEmpty(HttpContext.Session.GetInt32("idAdmin").ToString()))
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
