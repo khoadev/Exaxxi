c3119ae5b0aa5e6aeb0e981dd9370ece8b2f010b
Duy Tran <tdd310797@gmail.com>
2019-07-26 13:36:45 +0700

    Selling in User

--- Exaxxi/Exaxxi/Controllers/WebAPI/OrdersAPIController.cs
+++ Exaxxi/Exaxxi/Controllers/WebAPI/OrdersAPIController.cs
@@ -30,7 +30,9 @@ namespace Exaxxi.Controllers.WebAPI
         [Route("CountOrderBuying_Account/{idUser}")]
         public IActionResult CountOrderBuying_Account(int idUser)
         {
-            var count = _context.Orders.Where(p => p.id_user == idUser && p.status == 1).Count();
+            var count = _context.Orders
+                .Join(_context.Posts, a => a.id_post, b => b.id, (a, b) => new { a, b })
+                .Where(p => p.a.id_user == idUser && p.b.kind == 1).Count();
 
             return Ok(count);
         }
@@ -46,6 +48,34 @@ namespace Exaxxi.Controllers.WebAPI
                 .Select(g => new OrderViewModel {
                     items = g.f,
                     orders = g.e.c.a,
+                    size = g.e.d
+                });
+
+            return Ok(data);
+        }
+
+        [Route("CountOrderSelling_Account/{idUser}")]
+        public IActionResult CountOrderSelling_Account(int idUser)
+        {
+            var count = _context.Orders
+                .Join(_context.Posts, a => a.id_post, b => b.id, (a, b) => new { a, b })
+                .Where(p => p.a.id_user == idUser && p.b.kind == 2).Count();
+
+            return Ok(count);
+        }
+
+        [Route("TakeDetailSelling_Account/{kind}/{idUser}")]
+        public IActionResult TakeDetailSelling_Account(int kind, int idUser)
+        {
+            var data = _context.Orders
+                .Join(_context.Posts, a => a.id_post, b => b.id, (a, b) => new { a, b })
+                .Join(_context.Sizes, c => c.b.id_size, d => d.id, (c, d) => new { c, d })
+                .Join(_context.Items, e => e.d.id_item, f => f.id, (e, f) => new { e, f })
+                .Where(p => p.e.c.a.id_user == idUser && p.e.c.a.status == kind && p.e.c.b.kind == 2)
+                .Select(g => new OrderViewModel {
+                    items = g.f,
+                    orders = g.e.c.a,
+                    size = g.e.d
                 });
 
             return Ok(data);
