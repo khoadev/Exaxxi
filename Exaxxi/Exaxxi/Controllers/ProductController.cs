5e9f8b82a4c2902608a161a4ec3b63b0557314e8
Genos <42258883+nhatnam2405@users.noreply.github.com>
2019-07-30 12:15:30 +0700

    toi uu voucher

--- Exaxxi/Exaxxi/Controllers/ProductController.cs
+++ Exaxxi/Exaxxi/Controllers/ProductController.cs
@@ -126,7 +126,7 @@ namespace Exaxxi.Controllers
             return View();
         }
 
-        public IActionResult Confirm_Checkout(int id, int act, int? size)
+        public async Task<IActionResult> Confirm_CheckoutAsync(int id, int act, int? size)
         {
             //Buy
             if (act == 0)
@@ -169,6 +169,7 @@ namespace Exaxxi.Controllers
                 if (HttpContext.Session.GetInt32("ck_id_voucher") != null)
                 {
                     orders.id_voucher = HttpContext.Session.GetInt32("ck_id_voucher").Value;
+                    await _api.getAPI("api/VouchersAPI/UpdateCount/" + HttpContext.Session.GetInt32("ck_id_voucher").Value);
                 }
                 
                 
