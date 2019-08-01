0eff502185cdab52802b73fab0d5f89a108cb985
Genos <42258883+nhatnam2405@users.noreply.github.com>
2019-08-01 13:03:18 +0700

    mutilang lol

--- Exaxxi/Exaxxi/wwwroot/js/lang.js
+++ Exaxxi/Exaxxi/wwwroot/js/lang.js
@@ -4,14 +4,35 @@
     SIGNUP:'Sign Up',
     SIGNIN:'Sign In',
     SEEALL:'See All',
-    POPULAR:'Popular Products'
+    POPULAR: 'Popular Products',
+    BRANDNAME: "Brand's Name",
+    SORTBY: 'SORT BY',
+    DEFAULT: 'Default',
+    LOWESTASK: 'Lowest Ask',
+    LOWESTASKDE: "The 'Most Popular' are the products with the most sales over the past 72 hours.",
+    HIGHESTBID: "Highest Bid",
+    HIGHESTBIDDE: "The highest price someone is currently offering to pay for this product.",
+    CATENAME: "Category's Name",
+    SIZE: "Size",
+    PRICERANGE: "Price Range"
 };
 
 var translationsVI = {
     BROWSE: 'Danh Mục',
-    NEWS: 'TIn Tức',
+    NEWS: 'Tin Tức',
     SIGNUP: 'Đăng Nhập',
     SIGNIN: 'Đăng Ký',
     SEEALL: 'Tất Cả',
-    POPULAR: 'Hàng Bán Chạy'
+    POPULAR: 'Hàng Bán Chạy',
+    BRANDNAME: "Tên Hãng",
+    SORTBY: 'Sắp Xếp',
+    DEFAULT: 'Mặc Định',
+    LOWESTASK: 'Giá Mua Thấp Nhất',
+    LOWESTASKDE: "Con di me thang kahng",
+    HIGHESTBID: "Giá Bán Cao Nhất",
+    HIGHESTBIDDE: "Di me m kahng",
+    CATENAME: "Dòng Sản Phẩm",
+    SIZE: "Kích cỡ",
+    PRICERANGE: "Mức Giá"
+
 };
